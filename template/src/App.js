0bf2047bc1f0b92c8b642051bc891aef7f1ca495
ravshan01 <ravshanselimov1@gmail.com>
2021-01-16 21:27:32 +0500

    Update App.js

--- template/src/App.js
+++ template/src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 import Header from './layouts/Header/Header';
 import Footer from './layouts/Footer/Footer';
@@ -8,10 +8,19 @@ import './App.css';
 
 
 export default function App() {
+  const [mediaLoading, setMediaLoading] = useState(true);
+
+  useEffect( () => {
+    if ( document.readyState === 'complete' ) setMediaLoading(false);  
+    window.addEventListener('load', () => setMediaLoading(false));
+  }, [])
+
+
   return (
     <div className="App">
 
-
+      <Header />
+      <Footer />
 
     </div>
   );
