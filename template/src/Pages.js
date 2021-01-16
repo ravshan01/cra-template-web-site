e36079883d3cc024cc58b45ba0b8637d60384c4d
ravshan01 <ravshanselimov1@gmail.com>
2021-01-16 21:58:44 +0500

    Create Pages.js

---
+++ template/src/Pages.js
@@ -0,0 +1,23 @@
+import {Suspense, lazy} from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Loader from './components/Loader/Loader';
+
+const Home = lazy( () => import('./views/HomePage/HomePage') );
+const LogIn  = lazy( () => import('./views/LogInPage/LogInPage') );
+const SignUp = laxy( () => import('./views/SignUpPage/SignUpPage') );
+
+
+
+export default function Pages(props){
+  return(
+    <Suspense fallback={<Loader />}>
+      <Switch>
+        
+        <Route exact path="/" component={Home} />
+        <Route path="log-in" component={LogIn} />
+        <Route path="sign-up" component={SignUp} />
+
+      </Switch>
+    </Suspense>
+  )
+}
